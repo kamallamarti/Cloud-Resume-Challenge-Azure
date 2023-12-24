e979e0f6e5f5c9bfe71d4f4458ca5c4054590f54
kamallamarti <lamartikamal@hotmail.de>
2023-12-24 11:04:03 +0100

    added Infra

--- Backend/api/count_function/__init__.py
+++ Backend/api/count_function/__init__.py
@@ -25,7 +25,7 @@ def main(
         OutputDocument.set(updated_document)
 
     return func.HttpResponse(
-        json.dumps(response_body),
+        response_body,
         status_code=200,
         headers={"Content-Type": "application/json"},
     )
